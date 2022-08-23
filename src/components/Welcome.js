2e227bf42f0b95b8a0651514b7763a0fcd72468e
Sandesh Titarmare <102647203+sandeshgithubt@users.noreply.github.com>
2022-08-23 13:38:23 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,15 @@
 // write code for Welcome component here
+// write code for Welcome component here
+import React from 'react';
+
+function Welcome(props){
+    return(
+        <div>
+            <h2>Hey{props.name}</h2>
+        <h2>Welcome to Newton School.</h2>
+        </div>
+
+    )
+}
+
+export default Welcome;
